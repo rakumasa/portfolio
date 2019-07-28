cd5a0ee8b7f163b58f4fb8ce1db597f028c62e04
SATOSHI TOKUNO <RAKUMASA@HOTMAIL.COM>
2019-07-28 15:56:58 -0700

    Added sections

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Software Engineer.", "Full-stack Engineer." ,"Web Developer."],
+    strings: ["Software Engineer.", "Full stack Engineer." ,"Web Developer."],
     typeSpeed: 60,
     loop: true,
     startDelay: 1000,
@@ -57,5 +57,56 @@ $(document).ready(function() {
     }
 
   });
+
+  $("[data-fancybox]")
+.fancybox();
+
+$(".items").isotope({
+  filter: '*',
+  animationOptions: {
+    duration: 1500,
+    easing: 'linear',
+    queue: false
+  }
+})
+
+$("#filters a").click(function() {
+
+  $("#filters .current").removeClass("current");
+  $(this).addClass("current");
+
+  var selector = $(this).attr("data-filter");
+
+  $(".items").isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear',
+      queue: false
+    }
+  });
+
+  return false;
+});
+
+const nav = $("#navigation");
+const navTop = nav.offset().top;
+
+$(window).on("scroll", stickyNavigation);
+
+function stickyNavigation() {
+
+  var body = $("body");
+
+  if($(window).scrollTop() >= navTop) {
+    body.css("padding-top",nav.outerHeight() + "px");
+    body.addClass("fixedNav");
+  }
+  else {
+    body.css("padding-top",nav.outerHeight() + "px");
+    body.removeClass("fixedNav");
+  }
+
+}
 
 });
